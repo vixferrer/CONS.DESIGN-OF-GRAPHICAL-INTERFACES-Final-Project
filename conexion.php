9a8f78d22480c26642c7ac89b69e88237abcb635
Vix <vickyferrer98@gmail.com>
2019-04-16 19:05:14 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,19 @@
 <?php
-    $server="localhost";
-    $username="root";
-    $password="";
-    $db="carrito";
-    $con=mysql_connect($server, $username,$password) or die("no se pudo establecer la conexión");
-    $sdb=mysql_select_db($con,$db)or die("la base de datos no existe");
+$host = "ec2-54-75-230-253.eu-west-1.compute.amazonaws.com";
+$user = "acsuirsktfnamd";
+$password = "f7b1851d0f809a8419e1cb26c7cbd28acb0f1af8d539295cb2829a20c5de854b";
+$dbname = "d1ru6ujq85cesu";
+$port = "5432";
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
 ?>
