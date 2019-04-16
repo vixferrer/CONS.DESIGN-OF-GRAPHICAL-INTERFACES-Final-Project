af5920d3a0e89d3fa49d7fa708a8c7db0fb7e6b6
Vix <vickyferrer98@gmail.com>
2019-04-16 19:35:09 +0200

    Revert "detials"
    This reverts commit e7488dd31d0f410edf04798115009cf37652593f.

--- probador/Camisa/DetallesProductos/camisaCuadrosAm.php
+++ probador/Camisa/DetallesProductos/camisaCuadrosAm.php
@@ -91,7 +91,7 @@
 							<div class="col-md-6">
 								<div id="product-main-view">
 									<div class="product-view">
-										<img src="img/<?php echo $details['imagen'];?>">
+										<img src="img/<?php echo $f['imagen'];?>">
 									</div>
 									<div class="product-view">
 										<img src="img/camisaCuadrosMostaza2.jpg" alt="">
@@ -117,8 +117,8 @@
 									<div class="product-label">
 										<span>Nuevo</span>
 									</div>
-									<h2 class="product-name"><?php echo $details['nombre'];?></h2>
-									<h3 class="product-price"><?php echo $details['precio'];?></h3>
+									<h2 class="product-name"><?php echo $f['nombre'];?></h2>
+									<h3 class="product-price"><?php echo $f['precio'];?></h3>
 									<p style="color:rgb(50, 151, 3);"><strong style="color:black">Disponibilidad: </strong> En stock</p>
 									<div class="product-options">
 										<ul class="size-option">
