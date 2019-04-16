8b9c4768352e7b3a1c68f9f991ac6c6a6ca20562
Vix <vickyferrer98@gmail.com>
2019-04-16 19:15:58 +0200

    sdva

--- probador/Camisa/DetallesProductos/conexion.php
+++ probador/Camisa/DetallesProductos/conexion.php
@@ -14,6 +14,7 @@ try{
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
-echo 'Connection failed: ' . $e->getMessage();
+echo 'Connection failed: ' . $e->getMessage("ERROR xd");
 }
 ?>
+
