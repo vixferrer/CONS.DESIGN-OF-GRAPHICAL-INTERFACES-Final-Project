0e4641b4834d278fc485394c5f9bd7c52f0119e7
Vix <vickyferrer98@gmail.com>
2019-04-16 19:12:12 +0200

    hbjhhj

--- probador/Camisa/DetallesProductos/conexion.php
+++ probador/Camisa/DetallesProductos/conexion.php
@@ -1,10 +1,19 @@
 <?php
-    $server="ec2-54-75-230-253.eu-west-1.compute.amazonaws.com";
-    $username="acsuirsktfnamd";
-    $password="f7b1851d0f809a8419e1cb26c7cbd28acb0f1af8d539295cb2829a20c5de854b";
-    $db="d1ru6ujq85cesu";
-    //  or die("no se pudo establecer la conexión");
-    $dbconn = pg_connect($server, 5432, $db, $username, $password )or die("no se pudo establecer la conexión");
-    $sdb=mysqli_select_db($con,$db)or die("la base de datos no existe");
-    
+$host = "ec2-54-75-230-253.eu-west-1.compute.amazonaws.com";
+$user = "acsuirsktfnamd";
+$password = "f7b1851d0f809a8419e1cb26c7cbd28acb0f1af8d539295cb2829a20c5de854b";
+$dbname = "d1ru6ujq85cesu";
+$port = "5432";
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
 ?>
