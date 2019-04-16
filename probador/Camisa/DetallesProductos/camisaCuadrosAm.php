f3915bfc20b9d0102000733c49021d03d878e370
Vix <vickyferrer98@gmail.com>
2019-04-16 19:18:59 +0200

    hjb

--- probador/Camisa/DetallesProductos/camisaCuadrosAm.php
+++ probador/Camisa/DetallesProductos/camisaCuadrosAm.php
@@ -74,7 +74,7 @@
 	<div class="section">
 		<?php
 			include 'conexion.php';
-			$re=mysqli_query($con, "select * from camisas where id=2") or die(mysql_error());
+			$re=mysqli_query($pdo, "select * from camisas where id=2") or die(mysql_error());
 			while($f=mysqli_fetch_array($re)){
 				?>
 
