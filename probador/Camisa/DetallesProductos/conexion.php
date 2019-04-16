680654a5b4336328eb22a995f5bb317d7b957814
Adryend14 <32337632+Adryend14@users.noreply.github.com>
2019-04-16 18:43:01 +0200

    Update conexion.php

--- probador/Camisa/DetallesProductos/conexion.php
+++ probador/Camisa/DetallesProductos/conexion.php
@@ -1,8 +1,8 @@
 <?php
-    $server="localhost";
-    $username="root";
-    $password="";
-    $db="carrito";
+    $server="ec2-54-75-230-253.eu-west-1.compute.amazonaws.com";
+    $username="acsuirsktfnamd";
+    $password="f7b1851d0f809a8419e1cb26c7cbd28acb0f1af8d539295cb2829a20c5de854b";
+    $db="d1ru6ujq85cesu";
     $con=mysqli_connect($server, $username,$password) or die("no se pudo establecer la conexión");
     $sdb=mysqli_select_db($con,$db)or die("la base de datos no existe");
 ?>
