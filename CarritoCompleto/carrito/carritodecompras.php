82c3e84c28764296030e86dfc0b69268ff0e555b
Vix <vickyferrer98@gmail.com>
2019-04-17 12:43:30 +0200

    asc

--- CarritoCompleto/carrito/carritodecompras.php
+++ CarritoCompleto/carrito/carritodecompras.php
@@ -93,7 +93,7 @@
 						<img src="./productos/<?php echo $datos[$i]['Imagen'];?>"><br>
 						<span ><?php echo $datos[$i]['Nombre'];?></span><br>
 						<span>Precio: <?php echo $datos[$i]['Precio'];?></span><br>
-						<a href="restarCantidad.php"  class="aceptar" data-id="<?php echo $datos[$i]['Id']?>">Eliminar</a>
+						<a href="restarCantidad.php" class="eliminar" data-id="<?php echo $datos[$i]['Id']?>">Eliminar</a>
 				</div>
 			<?php
 				$total=($datos[$i]['Cantidad']*$datos[$i]['Precio'])+$total;
