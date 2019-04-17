af8d8d5036d25363bb49f51497dfa9fe3b0f7cf2
Vix <vickyferrer98@gmail.com>
2019-04-17 11:35:44 +0200

    commit 3

--- CarritoCompleto/carrito/index.php
+++ CarritoCompleto/carrito/index.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="utf-8"/>
-	<title>Carrito de Compras</title>
+	<title>Vestuario</title>
 	<link rel="stylesheet" type="text/css" href="./css/estilos.css">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 	<script type="text/javascript"  href="./js/scripts.js"></script>
