ff3327ec63819e2a602f778b898b52487bb58f56
Adryend14 <32337632+Adryend14@users.noreply.github.com>
2019-04-17 16:06:16 +0200

    Update carritodecompras.php

--- CarritoCompleto/carrito/carritodecompras.php
+++ CarritoCompleto/carrito/carritodecompras.php
@@ -93,7 +93,7 @@
 						<img src="./productos/<?php echo $datos[$i]['Imagen'];?>"><br>
 						<span ><?php echo $datos[$i]['Nombre'];?></span><br>
 						<span class="precio">Precio: <?php echo $datos[$i]['Precio'];?></span><br>
-						<span href="#" class="eliminar" data-id="<?php echo $datos[$i]['Id']?>">Eliminar</span>
+						<a href="#" class="eliminar" data-id="<?php echo $datos[$i]['Id']?>">Eliminar</a>
 				</div>
 			<?php
 				$total=($datos[$i]['Cantidad']*$datos[$i]['Precio'])+$total;
