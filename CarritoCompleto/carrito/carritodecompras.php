e98580188ba7b77b6f11bc195d414ee53e04e2be
Adryend14 <32337632+Adryend14@users.noreply.github.com>
2019-04-17 16:44:08 +0200

    ihihhi

--- CarritoCompleto/carrito/carritodecompras.php
+++ CarritoCompleto/carrito/carritodecompras.php
@@ -91,9 +91,11 @@
 	?>
 				<div class="producto">
 						<img src="./productos/<?php echo $datos[$i]['Imagen'];?>"><br>
-						<span class="nombreElemento"><?php echo $datos[$i]['Nombre'];?></span><br>
-						<span class="precio">Precio: <?php echo $datos[$i]['Precio'];?></span><br>
-						<a href="#" class="eliminar" data-id="<?php echo $datos[$i]['Id']?>">Eliminar</a>
+						<<a class= "nombreElemento" href="#">class="nombreElemento"><?php echo $datos[$i]['Nombre'];?></a><br>
+						<nav class="detallesYPrecio">
+							<span class="precio">Precio: <?php echo $datos[$i]['Precio'];?></span><br>
+							<a href="#" class="eliminar" data-id="<?php echo $datos[$i]['Id']?>">Eliminar</a>
+						</nav>
 				</div>
 			<?php
 				$total=($datos[$i]['Cantidad']*$datos[$i]['Precio'])+$total;
