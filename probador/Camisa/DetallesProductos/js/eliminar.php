55985c9afd801bc49560bacf2283d3608f3487af
Adryend14 <32337632+Adryend14@users.noreply.github.com>
2019-04-18 14:37:19 +0200

    awawa

--- probador/Camisa/DetallesProductos/js/eliminar.php
+++ probador/Camisa/DetallesProductos/js/eliminar.php
@@ -5,16 +5,16 @@ $dbname = "d1ru6ujq85cesu";
 	for($i=0;$i<count($arreglo);$i++){
 		if($arreglo[$i]['Id']!=$_POST['Id']){
 			$datosNuevos[]=array(
-				'Id'=>$arreglo[$i]['Id'],
-				'Nombre'=>$arreglo[$i]['Nombre'],
-				'Precio'=>$arreglo[$i]['Precio'],
-				'Imagen'=>$arreglo[$i]['Imagen'],
-				'Cantidad'=>$arreglo[$i]['Cantidad']
+				'Id'=>$arreglo[$i]['id'],
+				'Nombre'=>$arreglo[$i]['nombre'],
+				'Imagen'=>$arreglo[$i]['imagen'],
+				'Precio'=>$arreglo[$i]['precio']
 				);
 		}
 	}
 	if(isset($datosNuevos)){
 		$_SESSION[$dbname]=$datosNuevos;
+		echo '0';
 	}else{
 		unset($_SESSION[$dbname]);
 		echo '0';
