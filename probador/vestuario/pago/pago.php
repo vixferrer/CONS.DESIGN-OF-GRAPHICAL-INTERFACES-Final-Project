c411f0c18a83a5037be2b29167499c58aa270e21
Vix <vickyferrer98@gmail.com>
2019-04-20 16:26:29 +0200

    update

--- probador/vestuario/pago/pago.php
+++ probador/vestuario/pago/pago.php
@@ -260,15 +260,5 @@
 		<!-- /container -->
 	</div>
 	<!-- /section -->
-
-	<!-- jQuery Plugins -->
-	
-	<!-- <script src="js/jquery.min.js"></script> -->
-	<!-- <script src="js/bootstrap.min.js"></script> -->
-	<!-- <script src="js/slick.min.js"></script> -->
-	<!-- <script src="js/nouislider.min.js"></script> -->
-	<!-- <script src="js/jquery.zoom.min.js"></script> -->
-	<!-- <script src="js/main.js"></script> -->
-
 </body>
 </html>
