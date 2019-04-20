c9f1ff7d1ed614972264a5e37b67be1313ad2dc4
Vix <vickyferrer98@gmail.com>
2019-04-20 16:23:51 +0200

    scripts comentados

--- probador/vestuario/pago/pago.php
+++ probador/vestuario/pago/pago.php
@@ -262,12 +262,13 @@
 	<!-- /section -->
 
 	<!-- jQuery Plugins -->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/slick.min.js"></script>
-	<script src="js/nouislider.min.js"></script>
-	<script src="js/jquery.zoom.min.js"></script>
-	<script src="js/main.js"></script>
+	
+	<!-- <script src="js/jquery.min.js"></script> -->
+	<!-- <script src="js/bootstrap.min.js"></script> -->
+	<!-- <script src="js/slick.min.js"></script> -->
+	<!-- <script src="js/nouislider.min.js"></script> -->
+	<!-- <script src="js/jquery.zoom.min.js"></script> -->
+	<!-- <script src="js/main.js"></script> -->
 
 </body>
 </html>
