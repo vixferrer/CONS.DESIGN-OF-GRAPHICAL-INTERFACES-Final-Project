4054a49a28bbb6aa7ecd59ae3e611c30c2c711c0
Vix <vickyferrer98@gmail.com>
2019-04-21 22:24:07 +0200

    asd

--- probador/Abrigo/DetallesProductos/store.js
+++ probador/Abrigo/DetallesProductos/store.js
@@ -29,6 +29,6 @@ function updateCartTotal() {
         total = total + price
     }
     total = Math.round(total * 100) / 100
-    console.log(document.getElementByClassId('total'))
+    console.log(document.getElementByClassId('total').innerText)
     document.getElementByClassId('total').innerText = total + '€'
 }
