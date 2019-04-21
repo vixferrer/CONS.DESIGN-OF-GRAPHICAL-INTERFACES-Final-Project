beea802c324f4802ece7a4408f49705b3eb827ae
Adryend14 <32337632+Adryend14@users.noreply.github.com>
2019-04-21 16:20:20 +0200

    Update camisetaMarvel.php

--- probador/DetallesProductos/camisetaMarvel.php
+++ probador/DetallesProductos/camisetaMarvel.php
@@ -11,15 +11,17 @@
 
     <link rel="stylesheet" href="Barra/estilosB.css">
     <link rel="stylesheet" href="Barra/fonts/style.css">
+
     <link rel="stylesheet" href="css/estilos.css">
+
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+
     <script src="js/jquery-3.2.1.js"></script>
 	<script src="js/script.js"></script>
-
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="Barra/menu.js"></script>
 		
-		<script>
+	<script>
         function goTo(url) {
             window.location = url;
                  }
