97de2ffa2364e9392592f3ce816db95c84c630eb
Gonzalo <gonsez@gmail.com>
2019-04-21 16:45:10 +0200

    ñee

--- probador/DetallesProductos/camisetaMarvel.php
+++ probador/DetallesProductos/camisetaMarvel.php
@@ -11,15 +11,12 @@
 
     <link rel="stylesheet" href="Barra/estilosB.css">
     <link rel="stylesheet" href="Barra/fonts/style.css">
-
     <link rel="stylesheet" href="css/estilos.css">
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
 
     <script src="js/jquery-3.2.1.js"></script>
 	<script src="js/script.js"></script>
-	<script src="http://code.jquery.com/jquery-latest.js"></script>
-    
 		
 	<script>
         function goTo(url) {
@@ -47,6 +44,7 @@
     <!-- Custom stlylesheet -->
     <link type="text/css" rel="stylesheet" href="css/style.css" />
 
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="Barra/menu.js"></script>
 </head>
 
