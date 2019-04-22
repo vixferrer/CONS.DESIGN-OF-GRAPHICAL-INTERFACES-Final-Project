2d86db8fff9d4e89e874d2ebb08322d04f27ff9d
Vix <vickyferrer98@gmail.com>
2019-04-22 02:49:16 +0200

    probando

--- probador/Abrigo/DetallesProductos/carritodecompras.php
+++ probador/Abrigo/DetallesProductos/carritodecompras.php
@@ -117,7 +117,7 @@
 			}else{
 				echo '<center><h2>No has añadido ningun producto</h2></center>';
 			}
-			echo '<center><h2 id="total">Total:'.$total.'</h2></center>';
+			echo '<center><h2 id="total">Total:'.$total.'€</h2></center>';
 			
 		?>
 		<center><a href="../../../probador/index.php" class="volver aceptar" >Volver a tienda</a></center>
