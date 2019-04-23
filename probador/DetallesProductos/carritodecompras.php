7e729669e5b2b9d0c8ae5facf1cda251ba03fe8c
Gonzalo <gonsez@gmail.com>
2019-04-23 03:17:32 +0200

    ññlkñ

--- probador/DetallesProductos/carritodecompras.php
+++ probador/DetallesProductos/carritodecompras.php
@@ -110,7 +110,7 @@
                  }
         function pausa_alerta(espera) {
             alert("¡¡¡¡¡Vestuario listo!!!!!");
-            goTo('../../vestuario/probador.php');
+            goTo('../vestuario/probador.php');
                 }
     </script>
 
